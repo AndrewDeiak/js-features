3f6cefe9f941b9706ed8273428066659520570e6
AndrewDeiak <andrew.deiakk@gmail.com>
2020-12-23 18:11:48 +0200

    es2015 number, string

---
+++ src/es-2015/number-methods.js
@@ -0,0 +1,11 @@
+console.log(Number.isFinite(2));
+console.log(Number.isFinite(NaN));
+console.log(Number.isFinite(Infinity));
+
+console.log(Number.isNaN(NaN));
+console.log(Number.isNaN("string"));
+console.log(Number.isNaN(2.4));
+
+console.log(Number.isInteger(NaN));
+console.log(Number.isInteger(Infinity));
+console.log(Number.isInteger(2));
---
+++ src/es-2015/string-methods.js
@@ -0,0 +1,10 @@
+const name = "Andrew Deiak";
+
+console.log(name.startsWith("And"));
+console.log(name.startsWith("and"));
+
+console.log(name.endsWith("Deiak"));
+console.log(name.endsWith("sseiak"));
+
+console.log(name.includes("w D"));
+console.log(name.includes("Dei"));
